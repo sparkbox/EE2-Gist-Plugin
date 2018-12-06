0ad79a6de3f5d9dce414b333f78b5765dab5dfb8
Yosevu Kilonzo <yosevuk@gmail.com>
2018-12-06 10:57:56 -0500

    feat: Add updated plugin code

--- gist/pi.gist.php
+++ gist/pi.gist.php
@@ -1,59 +1,67 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
-* Plugin file for gist.
-* 
-* This file must be placed in the
-* /expressionengine/third_party/gist folder in your ExpressionEngine installation.
+* gist Class
 *
-* @package Gist
-* @version 1.0
-* @author Rob Harr <rob@heysparkbox.com>
-* @link http://olivierbon.com/projects/dreebbble
-* @copyright Copyright (c) 2011, Sparkbox
-* @license http://creativecommons.org/licenses/by-sa/3.0/ Creative Commons Attribution-Share Alike 3.0 Unported
+* @package     ExpressionEngine
+* @version     1.0
+* @category    Plugin
+* @author      Rob Harr <rob@heysparkbox.com>, Patrick Simpson <patrick@heysparkbox.com
+* @link        http://seesparkbox.com
+* @copyright   Copyright (c) 2014, Sparkbox
+* @license     http://creativecommons.org/licenses/by-sa/3.0/ Creative Commons Attribution-Share Alike 3.0 Unported
 */
 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+$plugin_info = array(
+    'pi_name'         => 'gist',
+    'pi_version'      => '1.1',
+    'pi_author'       => 'Rob Harr',
+    'pi_author_url'   => 'http://seesparkbox.com/',
+    'pi_description'  => 'Gets Gists',
+    'pi_usage'        => Gist::usage()
+);
+
 
 class Gist {
-  
-  var $return_data;
+
+  public $return_data = "";
 
   /**
-  * fetch the gist
-  **/
+   * fetch
+   *
+   * This function returns a gist code
+   *l
+   * @access  public
+   * @return  string
+   */
   public function fetch() {
-    $this->EE =& get_instance();  
-    $gistId = ( $this->EE->TMPL->fetch_param('id'));
+    $gistId = ( ee()->TMPL->fetch_param('id'));
     //the file is a optional param
-    $gistFile = ( $this->EE->TMPL->fetch_param('file'));
-    
+    $gistFile = ( ee()->TMPL->fetch_param('file'));
     //if there is no gist ID then we need to return nothing.
-    if(!isset($gistId) || empty($gistId)) { 
+    if(!isset($gistId) || empty($gistId)) {
       return $this->return_data;
     }
-
     $url = 'https://gist.github.com/'.$gistId.'.js';
-    
+
     //if a file is specified then lets append it to the end of the URL
     if(isset($gistFile) && !empty($gistFile)){
-      
+
        $url =  $url.'?file='.$gistFile;
-      
+
     }
-    
-    // go and get the contents of the URL that we just built. 
-    $result = file_get_contents($url);
-    
+
+    // go and get the contents of the URL that we just built.
+    $result = @file_get_contents($url);
+
     //if the result comes back with some content then we need to strip out the pesky javascript and linebreaks
-    if ( isset($result) && !empty($result) ) {     
+    if ( isset($result) && !empty($result) ) {
       $result = $this->format_return($result);
     }
-    
+
     $this->return_data = $result;
     return $this->return_data;
   }
-  
+
   //Format the return to get ride of the line breaks and the javascript
   function format_return($return) {
     $return = preg_replace( '/document.write\(\'/', '', $return );
@@ -63,7 +71,28 @@ class Gist {
     return $return;
   }
 
+    /**
+     * Usage
+     *
+     * This function describes how the plugin is used.
+     *
+     * @access  public
+     * @return  string
+     */
+    public static function usage()
+    {
+        ob_start();  ?>
+
+    To Get Gists
+    {exp:gist:fetch id='your_gist_id' file='js'}
+    <?php
+        $buffer = ob_get_contents();
+        ob_end_clean();
+
+        return $buffer;
+    }
+    // END
 }
 
 /* End of file pi.gist.php */
-/* Location: ./system/expressionengine/third_party/gist/pi.gist.php */
+/* Location: ./foundry/forgeIt/expressionengine/third_party/gist/pi.gist.php */
